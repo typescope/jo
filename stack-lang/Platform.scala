6aebc27877096e82febb86b35d4f8a2269fba23e
Fengyun Liu <fengyun.liu.cs@gmail.com>
2024-03-10 08:14:24 +0100

    Format code

--- stack-lang/Platform.scala
+++ stack-lang/Platform.scala
@@ -1,20 +1,15 @@
 import Assembly.*
 
 import IO.ByteBuffer
+import Sast.Symbol
 
 /**
-  * The platform abstraction encapsulates the logic about how
-  * to implement the value stack and call stack.
+  * The platform abstraction encapsulates implementation details about the value
+  * stack, call stack and primitive.
   *
-  * The value and call stack implementation is dependent on the
-  * underlying operating system (if present) and processor.
+  * The value stack, call stack and primitives are dependent on the underlying
+  * operating system (if present) and processor, or virtual machine.
   *
-  * The platform installs an initializer which will initialize
-  * the runtime environment, and then start executing the user
-  * entry point.
-  *
-  * - It also provides platform services such as exit, print, etc.
-  * - It encapsulates the details about generating the executable file.
   */
 abstract class Platform:
   /**
@@ -22,29 +17,29 @@ abstract class Platform:
     *
     * Calling the passed function will compile the user entry code.
     *
-    * The method enables platform to add code around the program entry.  For example, new code can
-    * be inserted before the entry to initialize the language runtime and after the entry to exit
-    * gracely.
+    * The method enables platform to add code around the program entry.  For
+    * example, new code can be inserted before the entry to initialize the
+    * language runtime and after the entry to exit gracely.
     */
   def entry(init: => Unit): Unit
 
   /** Declare the symbol to the platform as a preparation for compilation */
-  def declare(sym: Sast.Symbol): Unit
+  def declare(sym: Symbol): Unit
 
   /** Call the funtion */
-  def call(fun: Sast.Symbol): Unit
+  def call(fun: Symbol): Unit
 
   /** Initialize a value definition
     *
     * Calling the passed function will compile the initializer.
     */
-  def initVal(sym: Sast.Symbol, initializer: () => Unit): Unit
+  def initVal(sym: Symbol, initializer: () => Unit): Unit
 
   /** Compile a function
     *
     * Calling the passed function will compile the body of the function.
     */
-  def function(sym: Sast.Symbol, body: () => Unit): Unit
+  def function(sym: Symbol, body: () => Unit): Unit
 
   /** Push an integer literal to value stack */
   def push(v: Int): Unit
@@ -53,7 +48,7 @@ abstract class Platform:
   def push(v: Boolean): Unit
 
   /** Push the value associated with the given symbol to value stack */
-  def push(sym: Sast.Symbol): Unit
+  def push(sym: Symbol): Unit
 
   /** Push a procedure literal to value stack
     *
@@ -62,7 +57,7 @@ abstract class Platform:
   def push(proc: () => Unit): Unit
 
   /** Compile a primitive */
-  def primitive(sym: Sast.Symbol): Unit
+  def primitive(sym: Symbol): Unit
 
   /** Prepare to start the compilation */
   def start(): Unit
