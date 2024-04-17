29f3130c261d70fde19216ee821922694c6710e1
Fengyun Liu <fengyun.liu.cs@gmail.com>
2024-04-17 09:14:54 +0200

    Change graph representation for simpler coalescing

--- stack-lang/GraphColoring.scala
+++ stack-lang/GraphColoring.scala
@@ -20,59 +20,70 @@ object GraphColoring:
 
   /** Interference graph */
   class Graph(
-    var nodes: Set[Node],
-    var conflicts: Set[Edge],
-    var moves: Set[Edge]):
+    var conflicts: Map[Node, Set[Node]],
+    var moves: Map[Node, Set[Node]]):
 
     val actions = mutable.ArrayBuffer.empty[Action]
 
     def isMoveRelated(node: Node): Boolean =
-      moves.exists(e => e.node1 == node || e.node2 == node)
+      moves.contains(node)
 
-    def degree(node: Node): Int =
-      conflicts.filter(e => e.node1 == node || e.node2 == node).size
+    def degree(node: Node): Int = conflicts(node).size
 
     def conflict(node1: Node, node2: Node) =
-      conflicts.contains(Edge(node1, node2)) ||
-      conflicts.contains(Edge(node2, node1))
+      conflicts(node1).contains(node2)
 
     def simplify(node: Node): Unit =
-      assert(moves.forall(e => e.node1 != node && e.node2 != node))
+      assert(!moves.contains(node))
 
       actions += Action.Remove(node)
-      nodes -= node
-      conflicts = conflicts.filter(e => e.node1 != node && e.node2 != node)
+
+      val conflictees = conflicts(node)
+      conflicts -= node
+      for conflictee <- conflictees do
+        conflicts = conflicts.updated(conflictee, conflicts(conflictee) - node)
 
     def coalesce(node1: Node, node2: Node): Unit =
       val merged = new Node.Merged(node1, node2)
       actions += Action.Coalesce(merged)
 
-      nodes -= node1
-      nodes -= node2
-      nodes += merged
-      moves -= Edge(node1, node2)
-      moves -= Edge(node2, node1)
+      def replace(node: Node): Node =
+        if node == node1 || node == node2 then merged else node
 
-      def replace(edge: Edge): Edge =
-        if edge.node1 == node1 || edge.node1 == node2 then
-          Edge(merged, edge.node2)
-        else if edge.node2 == node1 || edge.node2 == node2 then
-          Edge(edge.node1, merged)
-        else
-          edge
+      // update conflicts
 
-      conflicts = conflicts.map(replace)
-      moves = moves.map(replace)
+      val conflictees1 = conflicts(node1)
+      val conflictees2 = conflicts(node2)
+      conflicts -= node1
+      conflicts -= node2
+      conflicts = conflicts.updated(merged, conflictees1.union(conflictees2))
 
-      // TODO: dedup in moves and conflicts
+      for conflictee <- conflictees1 do
+        conflicts = conflicts.updated(conflictee, conflicts(conflictee).map(replace))
 
+      for conflictee <- conflictees2 do
+        conflicts = conflicts.updated(conflictee, conflicts(conflictee).map(replace))
+
+      // update moves
+
+      val targets1 = moves(node1)
+      val targets2 = moves(node2)
+      moves -= node1
+      moves -= node2
+      moves = moves.updated(merged, targets1.union(targets2))
+
+      for target <- targets1 if target != node2 do
+        moves = moves.updated(target, moves(target).map(replace))
+
+      for target <- targets2 if target != node1 do
+        moves = moves.updated(target, moves(target).map(replace))
 
   case class Result(assignment: Map[Int, Int], spillings: List[Int])
 
   def build(result: Liveness.Result): Graph =
     val nodeMap = mutable.Map.empty[Int, Node]
-    val conflicts = mutable.Set.empty[Edge]
-    val moves = mutable.Set.empty[Edge]
+    val conflicts = mutable.Map.empty[Node, Set[Node]]
+    val moves = mutable.Map.empty[Node, Set[Node]]
 
     for (_, liveSet) <- result.liveSets if liveSet.nonEmpty do
       val reg1 = liveSet.head
@@ -80,25 +91,23 @@ object GraphColoring:
       val node1 = nodeMap.getOrElseUpdate(reg1, Node.Single(reg1))
       for reg2 <- remains do
         val node2 = nodeMap.getOrElseUpdate(reg2, Node.Single(reg2))
-        // Avoid adding both (a, b) and (b, a)
-        conflicts -= Edge(node2, node1)
-        conflicts += Edge(node1, node2)
+        conflicts(node1) = conflicts.getOrElseUpdate(node1, Set.empty) + node2
+        conflicts(node2) = conflicts.getOrElseUpdate(node2, Set.empty) + node1
       end for
 
     for (reg1, toSet) <- result.liveSets do
       val node1 = nodeMap.getOrElseUpdate(reg1, Node.Single(reg1))
       for reg2 <- toSet do
         val node2 = nodeMap.getOrElseUpdate(reg2, Node.Single(reg2))
-        // Avoid adding both (a, b) and (b, a)
-        moves -= Edge(node2, node1)
-        moves += Edge(node1, node2)
+        moves(node1) = moves.getOrElseUpdate(node1, Set.empty) + node2
+        moves(node2) = moves.getOrElseUpdate(node2, Set.empty) + node1
       end for
 
-    Graph(nodeMap.values.toSet, conflicts.toSet, moves.toSet)
+    Graph(conflicts.toMap, moves.toMap)
 
   def simplify(graph: Graph, k: Int): Boolean =
     var simplified = false
-    for node <- graph.nodes do
+    for node <- graph.conflicts.keys do
       if graph.isMoveRelated(node) && graph.degree(node) < k then
         simplified = true
         graph.simplify(node)
@@ -107,14 +116,15 @@ object GraphColoring:
 
   def coalesce(graph: Graph, k: Int): Boolean =
     var coalesced = false
-    for Edge(node1, node2) <- graph.moves do
-      if
-        !graph.conflict(node1, node2) &&
-        graph.degree(node1) + graph.degree(node2) < k
-      then
-        // TODO: node1 and node2 conflict implies bad generated code
-        coalesced = true
-        graph.coalesce(node1, node2)
+    for (node1, targets) <- graph.moves do
+      for node2 <- targets do
+        if
+          !graph.conflict(node1, node2) &&
+          graph.degree(node1) + graph.degree(node2) < k
+        then
+          // TODO: node1 and node2 conflict implies bad generated code
+          coalesced = true
+          graph.coalesce(node1, node2)
     end for
 
     coalesced
