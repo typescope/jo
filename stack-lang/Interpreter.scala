36fc6a56ff6753fbfb3e53e9f9c85d38cd9c8be2
Fengyun Liu <fengyun.liu.cs@gmail.com>
2024-07-20 08:44:24 +0200

    AST interpreter

--- stack-lang/Interpreter.scala
+++ stack-lang/Interpreter.scala
@@ -6,67 +6,80 @@
 
 import scala.collection.mutable
 
-import Sast.*
-import Symbols.*
-
-def err(msg: String) = throw new Exception(msg)
+import Ast.*
+import Symbols.{ predef, runtime }
 
 /***********************************************************************
  *
  * Value Definitions
  *
  ***********************************************************************/
+object Interpreter:
+  import Denotation.*
 
-class ValueStack:
-  val stack: mutable.ArrayBuffer[Value] = new mutable.ArrayBuffer
+  def err(msg: String) = throw new Exception(msg)
 
-  def pop(): Value =
-    if stack.nonEmpty then stack.remove(stack.size - 1)
-    else err("Stack is empty")
+  class ValueStack:
+    private val stack: mutable.ArrayBuffer[Value] = new mutable.ArrayBuffer
 
-  def push(v: Value): Unit = stack.append(v)
+    def pop(): Value =
+      if stack.nonEmpty then stack.remove(stack.size - 1)
+      else err("Stack is empty")
 
-  def show: String = stack.toString
+    def push(v: Value): Unit = stack.append(v)
 
-sealed abstract class Denotation
+    def take: List[Value] =
+      val res = stack.toList
+      stack.clear()
+      res
 
-enum Value extends Denotation:
-  case IntVal(value: Int)
-  case BoolVal(value: Boolean)
-  case RecordVal(fields: Map[String, Value])
-  case FunVal(fun: Sast.FunDef, scope: Scope)
+    def size: Int = stack.size
 
-object Uninit extends Denotation
-case class PrimAction(op: ValueStack => Unit) extends Denotation
+    def show: String = stack.toString
 
-enum Scope:
-  case RootScope()
-  case NestedScope(outer: Scope)
+  enum Denotation:
+    case IntVal(value: Int)
+    case BoolVal(value: Boolean)
+    case RecordVal(fields: Map[String, Value])
+    case VariantVal(tag: String, values: List[Value])
+    case ClosureVal(lambda: Lambda, sc: Scope)
+    case FunCall(fun: FunDef, scope: Scope)
+    case PrimAction(op: ValueStack => Unit)
 
-  private val map: mutable.Map[Symbol, Denotation] = mutable.Map.empty
+  type Value = IntVal | BoolVal | RecordVal | VariantVal | ClosureVal
 
-  def resolve(sym: Symbol): Denotation =
-    map.get(sym) match
-      case None =>
+  //----------------------------------------------------------------------------
+
+  enum Scope:
+    case RootScope()
+    case NestedScope(outer: Scope)
+
+    private val map: mutable.Map[String, Denotation] = mutable.Map.empty
+
+    def fresh(): Scope = new NestedScope(this)
+
+    def resolve(name: String): Denotation =
+      map.get(name) match
+        case None =>
+          this match
+            case NestedScope(outer) => outer.resolve(name)
+            case _ => throw new Exception("Not found " + name)
+
+        case Some(res)  => res
+
+    def update(name: String, denot: Denotation): Unit =
+      if map.contains(name) then
+        map(name) = denot
+      else
         this match
-          case NestedScope(outer) => outer.resolve(sym)
-          case _ => throw new Exception("Not found " + sym)
+          case NestedScope(outer) => outer.update(name, denot)
+          case _ => err("Unknown name to update: " + name)
 
-      case Some(res)  => res
+    def bind(name: String, denot: Denotation): Unit =
+      assert(!map.contains(name), "Double binding")
+      map(name) = denot
 
-  def update(sym: Symbol, denot: Denotation): Unit =
-    if map.contains(sym) then
-      map(sym) = denot
-    else
-      this match
-        case NestedScope(outer) => outer.update(sym, denot)
-        case _ => err("Unknown name to update: " + sym.name)
-
-  def bind(sym: Symbol, denot: Denotation): Unit =
-    map(sym) = denot
-
-object Primitive:
-  import Value.*
+  //----------------------------------------------------------------------------
 
   def int2(op: (Int, Int) => Int)(vs: ValueStack): Unit =
     (vs.pop(), vs.pop()) match
@@ -119,90 +132,149 @@ object Primitive:
     val IntVal(v) = vs.pop(): @unchecked
     throw new Exception(v.toString)
 
-  val operators: Map[Symbol, ValueStack => Unit] = Map(
-      predef.add    ->    add,
-      predef.sub    ->    sub,
-      predef.mul    ->    mul,
-      predef.div    ->    div,
-      predef.mod    ->    mod,
-      predef.gt     ->    gt,
-      predef.lt     ->    lt,
-      predef.ge     ->    ge,
-      predef.le     ->    le,
-      predef.srl    ->    srl,
-      predef.sll    ->    sll,
-      predef.land   ->    land,
-      predef.lor    ->    lor,
-      predef.lxor   ->    lxor,
-      predef.band   ->    band,
-      predef.bor    ->    bor,
-      predef.bnot   ->    bnot,
-      predef.eql    ->    eql,
-      predef.p      ->    print,
-      runtime.abort ->    abort
+  val primitiveOperators: Map[String, ValueStack => Unit] = Map(
+      predef.add.name    ->    add,
+      predef.sub.name    ->    sub,
+      predef.mul.name    ->    mul,
+      predef.div.name    ->    div,
+      predef.mod.name    ->    mod,
+      predef.gt.name     ->    gt,
+      predef.lt.name     ->    lt,
+      predef.ge.name     ->    ge,
+      predef.le.name     ->    le,
+      predef.srl.name    ->    srl,
+      predef.sll.name    ->    sll,
+      predef.land.name   ->    land,
+      predef.lor.name    ->    lor,
+      predef.lxor.name   ->    lxor,
+      predef.band.name   ->    band,
+      predef.bor.name    ->    bor,
+      predef.bnot.name   ->    bnot,
+      predef.eql.name    ->    eql,
+      predef.p.name      ->    print,
+      runtime.abort.name ->    abort
   )
 
-object Interpreter:
+  //----------------------------------------------------------------------------
+
   def exec(prog: Prog): Unit =
     val rootScope = new Scope.RootScope()
 
-    for (sym, op) <- Primitive.operators do
+    for (sym, op) <- primitiveOperators do
       rootScope.bind(sym, PrimAction(op))
 
-    val sc = new Scope.NestedScope(rootScope)
-    for fun <- prog.funs do
-      sc.bind(fun.symbol, Value.FunVal(fun, sc))
+    val sc = rootScope.fresh()
+    for case fdef: FunDef <- prog.defs do
+      sc.bind(fdef.name, FunCall(fdef, sc))
 
     val vs = new ValueStack
-    for ValDef(sym, rhs) <- prog.vals do
-      exec(rhs)(using vs, sc)
-      sc.bind(sym, vs.pop())
+    for case vdef: ValDef <- prog.defs do
+      exec(vdef.rhs)(using vs, sc)
+      sc.bind(vdef.name, vs.pop())
 
     exec(prog.main)(using vs, sc)
 
-  def exec(phrase: Phrase)(using ValueStack, Scope): Unit =
-    for word <- phrase.words do exec(word)
+  def exec(phrase: Phrase)(using vs: ValueStack, sc: Scope): Unit =
+    val vs2 = new ValueStack
+
+    def processCall(words: List[Word]): Unit =
+      if vs2.size == 1 then
+        vs2.pop() match
+          case ClosureVal(lam, sc2) =>
+            // always call the first function in phrase
+            val paramCount = lam.params.size
+            assert(words.size >= paramCount, "words = " + words + ", paramCount = " + paramCount)
+            for arg <- words.take(paramCount) do exec(arg)(using vs2, sc2)
+            call(lam)(using vs2, sc2)
+            processCall(words.drop(paramCount))
+
+          case value =>
+            vs2.push(value)
+            process(words)
+      else
+        process(words)
+
+    def process(words: List[Word]): Unit =
+      words match
+        case word :: rest =>
+          exec(word)
+          processCall(rest)
+        case Nil =>
+    end process
+
+    process(phrase.words)
+
+    for value <- vs2.take do vs.push(value)
 
   def call(fdef: FunDef)(using vs: ValueStack, sc: Scope): Unit =
-    val funScope = new Scope.NestedScope(sc)
+    val funScope = sc.fresh()
     for param <- fdef.params.reverse do
-      funScope.bind(param, vs.pop())
-    for param <- fdef.locals do
-      funScope.bind(param, Uninit)
+      funScope.bind(param.name, vs.pop())
     exec(fdef.body)(using vs, funScope)
 
-  def exec(word: Word)(using vs: ValueStack, sc: Scope): Unit = Debug.trace(Printing.show(word) + ", stack = " + vs.show, enable = false):
+  def call(lam: Lambda)(using vs: ValueStack, sc: Scope): Unit =
+    val lamScope = sc.fresh()
+    for param <- lam.params.reverse do
+      lamScope.bind(param.name, vs.pop())
+    exec(lam.body)(using vs, lamScope)
+
+  def exec(value: Value, cases: List[Case])(using vs: ValueStack, sc: Scope): Unit =
+    val VariantVal(tag, values) = value: @unchecked
+    def matches(caseDef: Case): Boolean =
+      caseDef.pat match
+        case Wildcard()  => true
+        case TagPat(tagId, _) => tagId.name == tag
+
+    val Some(Case(pat, body)) = cases.find(matches): @unchecked
+
+    val caseScope = sc.fresh()
+    pat match
+      case TagPat(_, bindings) =>
+        assert(bindings.size == values.size)
+        for (id, value) <- bindings.zip(values) do
+          caseScope.bind(id.name, value)
+
+      case Wildcard()  =>
+
+    exec(body)(using vs, caseScope)
+
+  def eval(word: Word)(using vs: ValueStack, sc: Scope): Value =
+    exec(word)
+    vs.pop()
+
+  def exec(word: Word)(using vs: ValueStack, sc: Scope): Unit =
     word match
-      case IntLit(v)  => vs.push(Value.IntVal(v))
+      case IntLit(v)  => vs.push(IntVal(v))
 
-      case BoolLit(v) => vs.push(Value.BoolVal(v))
-
-      case Encoded(repr) => exec(repr)
+      case BoolLit(v) => vs.push(BoolVal(v))
 
       case RecordLit(args) =>
         val fieldValues = mutable.Map.empty[String, Value]
-        for (name, arg) <- args do
-          exec(arg)
-          fieldValues(name) = vs.pop()
-        vs.push(Value.RecordVal(fieldValues.toMap))
+        for NamedArg(id, arg) <- args do
+          fieldValues(id.name) = eval(arg)
+        vs.push(RecordVal(fieldValues.toMap))
 
       case Select(qual, name) =>
-        exec(qual)
-        val Value.RecordVal(fieldVals) = vs.pop(): @unchecked
+        val RecordVal(fieldVals) = eval(qual): @unchecked
         vs.push(fieldVals(name))
 
-      case Assign(sym, words) =>
-        exec(words)
-        sc.update(sym, vs.pop())
+      case Variant(tag, words, _) =>
+        val values = mutable.ArrayBuffer.empty[Value]
+        for word <- words do values += eval(word)
+        vs.push(VariantVal(tag.name, values.toList))
+
+      case Ast.Match(scrut, cases) =>
+        exec(eval(scrut), cases)
+
+      case Assign(id, rhs) =>
+        sc.update(id.name, eval(rhs))
 
       case If(cond, thenp, elsep) =>
-        exec(cond)
-        val Value.BoolVal(b) = vs.pop(): @unchecked
+        val BoolVal(b) = eval(cond): @unchecked
         if b then exec(thenp) else exec(elsep)
 
       case While(cond, body) =>
-        exec(cond)
-        val Value.BoolVal(b) = vs.pop(): @unchecked
+        val BoolVal(b) = eval(cond): @unchecked
         if b then
           exec(body)
           exec(word)
@@ -210,36 +282,20 @@ object Interpreter:
       case phrase: Phrase =>
         exec(phrase)
 
-      case Ident(sym) =>
-        sc.resolve(sym) match
+      case Ident(name) =>
+        sc.resolve(name) match
           case PrimAction(op) => op(vs)
+          case FunCall(fdef, sc) => call(fdef)(using vs, sc)
+          case value: Value => vs.push(value)
 
-          case fval @ Value.FunVal(fdef, sc2) =>
-            if sym.isFunction then
-              call(fdef)(using vs, sc2)
-            else
-              vs.push(fval)
+      case vdef: ValDef =>
+        sc.bind(vdef.name, eval(vdef.rhs))
 
-          case Uninit =>
-            err("Accessing uninitialized variable " + sym)
-
-          case value: Value =>
-            vs.push(value)
-
-      case ValDef(sym, rhs) =>
-        exec(rhs)
-        sc.bind(sym, vs.pop())
-
-      case Apply(fun, args) =>
+      case TypeApply(fun, _) =>
         exec(fun)
-        val funVal = vs.pop()
-        val argVals =
-          for arg <- args yield
-            exec(arg)
-            vs.pop()
 
-      case fdef: FunDef =>
-        sc.bind(fdef.symbol, Value.FunVal(fdef, sc))
+      case lam @ Lambda(params, body) =>
+        vs.push(ClosureVal(lam, sc))
 
 /***********************************************************************
  *
@@ -250,6 +306,4 @@ object Interpreter:
 def eval(file: String) = Reporter.monitor(file):
   IO.fileContent(file)        |>
   Parsing.parse               |>
-  Namer.transform             |+
-  Debug.peek(enable = false)  |>
   Interpreter.exec
