b7b63e0e5cd7b50b688a4d4822d26a23576aa9e0
Fengyun Liu <fengyun.liu.cs@gmail.com>
2024-11-26 12:18:28 +0100

    Better pointer for double definition

--- stack-lang/Diagnostics.scala
+++ stack-lang/Diagnostics.scala
@@ -37,14 +37,16 @@ object Diagnostics:
     override def toString() =
       val message = "Redefinition of " + symBefore.name
       val pos = symNow.sourcePos
-      val isOneLine = pos.isOneLine
       val lineContent = pos.source.readLine(pos.startLine).replaceAll("[\n\r]$", "")
       val padding = " " * pos.startLineColumn
       val num = if pos.length == 0 then 1 else pos.length
-      val pointer = if isOneLine then "^" * num  else "^"
+      val pointer = if pos.isOneLine then "^" * num else "^"
 
       val pos2 = symBefore.sourcePos
       val lineContent2 = pos2.source.readLine(pos2.startLine).replaceAll("[\n\r]$", "")
+      val num2 = if pos2.length == 0 then 1 else pos2.length
+      val padding2 = " " * pos2.startLineColumn
+      val pointer2 = if pos2.isOneLine then "^" * num2 else "^"
 
       s"""|---------- $kind at $pos ---------------
           || $lineContent
@@ -52,4 +54,5 @@ object Diagnostics:
           || $padding$message
           ||
           || The name `${symNow.name}` is already defined at $pos2:
-          || $lineContent2""".stripMargin
+          || $lineContent2
+          || $padding2$pointer2""".stripMargin
