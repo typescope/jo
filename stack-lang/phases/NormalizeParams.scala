06df416fa103570b229a706ff013f569075c0fc4
Fengyun Liu <fengyun.liu.cs@gmail.com>
2025-02-14 09:17:53 +0100

    Check permision of effects

--- stack-lang/phases/NormalizeParams.scala
+++ stack-lang/phases/NormalizeParams.scala
@@ -1,9 +1,11 @@
 package phases
 
+import ast.Positions.Source
 import sast.*
 import sast.Sast.*
 import sast.Symbols.*
 import typing.EffectAnalysis
+import reporting.Reporter
 
 /** This phase normalize usage of context parameters
   *
@@ -11,28 +13,29 @@ import typing.EffectAnalysis
   * - All transitive captures of context parameters are made explicit in objects
   * - Checks are performed for `allow`-clauses
   */
-class NormalizeParams extends Phase[EffectAnalysis.Cache]:
+class NormalizeParams(using Reporter) extends Phase[NormalizeParams.Context]:
   val contextObject = NormalizeParams.CacheContext
 
   override def transform(nss: List[Namespace]): List[Namespace] =
-    given cache: Context = contextObject.newContext()
+    given ctx: Context = contextObject.newContext()
     for
       ns <- nss
       case fdef: FunDef <- ns.defs
     do
-      cache.code(fdef.symbol) = fdef
+      ctx.cache.code(fdef.symbol) = fdef
 
     for ns <- nss yield transformNamespace(ns)
 
-  override  def transformFunDef(fdef: FunDef)(using Context): FunDef =
+  override  def transformFunDef(fdef: FunDef)(using ctx: Context): FunDef =
     if !fdef.symbol.isLocal && fdef.name == "main" then
-      val effs = EffectAnalysis.effects(fdef.symbol).filter(_.is(Flags.Default))
+      val effs = EffectAnalysis.effects(fdef.symbol)(using ctx.cache)
       val fdef2 = super.transformFunDef(fdef)
 
-      if effs.isEmpty then fdef2
+      val defaultEffs = effs.filter(_.is(Flags.Default))
+      if defaultEffs.isEmpty then fdef2
       else
         val span = fdef.body.span
-        val args = effs.toList.map: eff =>
+        val args = defaultEffs.toList.map: eff =>
           val defaultFunSym = eff.defaultFunction
           val paramRef = Ident(eff)(span)
           val defaultFunRef = Ident(defaultFunSym)(span)
@@ -45,8 +48,22 @@ class NormalizeParams extends Phase[EffectAnalysis.Cache]:
     else
       super.transformFunDef(fdef)
 
-object NormalizeParams:
+  override  def transformWith(withExpr: With)(using ctx: Context): Word =
+    withExpr.allow match
+      case Some(ids) =>
+        val allowed = ids.map(_.symbol)
+        val effs = EffectAnalysis.effects(withExpr.expr)(using ctx.cache)
+        val notAllowed = effs -- allowed
+        if notAllowed.nonEmpty then
+          given Source = ctx.owner.sourcePos.source
+          val list = notAllowed.mkString(", ")
+          Reporter.error("More context parameters used than allowed: " + list, withExpr.expr.pos)
+      case _ =>
+    end match
+    withExpr
 
-  object CacheContext extends Phase.ContextObject[EffectAnalysis.Cache]:
-    def newContext(owner: Symbol, old: EffectAnalysis.Cache) = old
-    def newContext() = EffectAnalysis.Cache()
+object NormalizeParams:
+  class Context(val cache: EffectAnalysis.Cache, val owner: Symbol)
+  object CacheContext extends Phase.ContextObject[Context]:
+    def newContext(owner: Symbol, old: Context) = Context(old.cache, owner)
+    def newContext() = Context(EffectAnalysis.Cache(), null)
