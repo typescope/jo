cf848b2d624c30f1774f09650d58c1df51fcbbde
Fengyun Liu <fengyun.liu.cs@gmail.com>
2025-02-22 00:17:57 +0100

    Fix #131: Improve expression typing

--- stack-lang/typing/ExprTyper.scala
+++ stack-lang/typing/ExprTyper.scala
@@ -53,21 +53,29 @@ object ExprTyper:
     /** An typed word */
     case Typed(word: Word)
 
-    /** A call tree where the function is a typed ident */
+    /** A call tree where the function is a typed ref tree */
     case Call(fun: Word, preArgs: List[Item], postArgs: List[Item])
              (val preTypes: List[Type], val postTypes: List[Type], val resultType: Type)
+
+    /** A dotless infix method call */
+    case InfixCall(obj: Item, method: Ast.Ident, arg: Item)
 
     def span: Span =
       this match
         case Raw(word) => word.span
+
         case Typed(word) => word.span
+
         case Call(fun, pre, post) =>
           (pre ++ post).foldRight(fun.span)(_.span | _)
+
+        case InfixCall(obj, method, arg) =>
+          obj.span | method.span | arg.span
 
 /**
   * Perform type checking for an expression.
   *
-  * Used to check stack safety and construct function call nodes.
+  * Used to construct function call nodes.
   *
   * Instance of the class should be able to be reused to type check different
   * expression. Therefore, it should not contain any expression-specific state.
@@ -76,17 +84,53 @@ class ExprTyper(namer: Namer, checker: Checker, inferencer: Inferencer):
   import ExprTyper.Item
 
   def transform(expr: Ast.Expr)(using  sc: Scope, rp: Reporter, so: Source, tt: TargetType): Word =
-    assert(expr.words.nonEmpty)
+    expr.words: @unchecked match
+    case head :: Nil =>
+      namer.transform(head)
 
-    val words = mutable.ListBuffer.from(expr.words)
-    val values = parse(words, -1)
+    case head :: rest =>
+      given TargetType = TargetType.Unknown
+      val wordTyped = namer.transform(head)
+      val tp = wordTyped.tpe
 
-    if values.size > 1 then
-      val rest = values.init
-      val span = rest.head.span | rest.last.span
-      Reporter.error("Found extra value, an expression should produce at most one value", span.toPos)
+      def isDotlessMethodCallPattern() = tp.isObjectType && rest.head.match
+        case Ast.Ident(name) => tp.termMember(name).isProcType
+        case _ => false
 
-    typeItem(values.last)
+      if tp.isError then
+        wordTyped
+
+      else if isDotlessMethodCallPattern() then
+        // Dotless method call pattern, where the infix operator takes exactly one parameter
+        val words = mutable.ListBuffer.from(expr.words)
+        val item = parseDotless(words, -1)
+
+        assert(words.isEmpty, words)
+        typeItem(item)
+
+      else if tp.hasApplyMethod then
+        // function apply pattern, all remaing words are arguments
+        val procType = tp.termMember("apply").asProcType
+        val preTypes = procType.preParamTypes
+        val postTypes = procType.postParamTypes
+        val resultType = procType.resultType
+
+        val autoApply = Select(wordTyped, "apply")(procType, wordTyped.span)
+        val args = for word <- rest yield Item.Raw(word)
+        val item = Item.Call(autoApply, preArgs = Nil, postArgs = args)(preTypes, postTypes, resultType)
+        typeItem(item)
+
+      else
+        // mixed prefix/infix/postfix pattern, arity depends on type of the function
+        val words = mutable.ListBuffer.from(expr.words)
+        val values = parseMixed(words, -1)
+
+        if values.size > 1 then
+          val rest = values.init
+          val span = rest.head.span | rest.last.span
+          Reporter.error("Found extra value, an expression should produce at most one value", span.toPos)
+
+        typeItem(values.last)
   end transform
 
   private def typeItem(item: Item)(using sc: Scope, rp: Reporter, so: Source, tt: TargetType): Word =
@@ -98,13 +142,13 @@ class ExprTyper(namer: Namer, checker: Checker, inferencer: Inferencer):
 
       case call @ Item.Call(fun, preArgs, postArgs) =>
         val span = call.span
-        if preArgs.size < call.preTypes.size then
+        if preArgs.size != call.preTypes.size then
           Reporter.error(
             s"Function ${fun.show} expects ${call.preTypes.size} pre arguments, found = ${preArgs.size}",
             span.toPos)
           errorTree(span)
 
-        else if postArgs.size < call.postTypes.size then
+        else if postArgs.size != call.postTypes.size then
           Reporter.error(
             s"Function ${fun.show} expects ${call.postTypes.size} post arguments, found = ${postArgs.size}",
             span.toPos)
@@ -124,8 +168,45 @@ class ExprTyper(namer: Namer, checker: Checker, inferencer: Inferencer):
           val word = Apply(fun, preArgs2 ++ postArgs2)(call.resultType, span)
           checker.adapt(word, tt)
 
-  /** Parse items from the words with the limit precedence */
-  private def parse(words: mutable.ListBuffer[Ast.Word], precLimit: Int)(using rp: Reporter, sc: Scope, so: Source): List[Item] =
+      case Item.InfixCall(obj, meth, arg) =>
+        val objWord = typeItem(obj)
+        val objType = objWord.tpe
+        val objSpan = obj.span
+
+        if objType.isObjectType then
+          // TODO: support poly method type
+          objType.getTermMember(meth.name) match
+            case Some(tp) =>
+              if tp.isProcType then
+                val procType = tp.asProcType
+                val paramSize = procType.paramTypes.size
+                if paramSize != 1 then
+                  Reporter.error(
+                    s"The method ${meth.name} takes ${paramSize} parameters. The dotless call syntax only supports methods of one parameter",
+                    meth.span.toPos)
+                  errorTree(meth.span)
+                else
+                  val argTyped =
+                    given TargetType = TargetType.Known(procType.paramTypes.head)
+                    typeItem(arg)
+
+                  val fun = Select(objWord, meth.name)(procType, objSpan | meth.span)
+                  val word = Apply(fun, argTyped :: Nil)(procType.resultType, item.span)
+                  checker.adapt(word, tt)
+              else
+                Reporter.error( s"The member ${meth.name} is not a method", meth.pos)
+                errorTree(meth.span)
+
+            case None =>
+              Reporter.error( s"Object of the type ${objType.show} does not have member ${meth.name}", objSpan.toPos)
+              errorTree(objSpan)
+        else
+          Reporter.error(s"Object type expected, found = " + objWord.tpe.show, objSpan.toPos)
+          errorTree(objSpan)
+
+
+  /** Parse items from the words with the limit precedence for mixed prefix/infix/postfix pattern */
+  private def parseMixed(words: mutable.ListBuffer[Ast.Word], precLimit: Int)(using rp: Reporter, sc: Scope, so: Source): List[Item] =
     // println("Parsing " + words + ", precedence = " + precedence)
 
     val values = mutable.ArrayBuffer.empty[Item]
@@ -144,7 +225,6 @@ class ExprTyper(namer: Namer, checker: Checker, inferencer: Inferencer):
           if wordTyped.tpe.isPolyType then
             val polyType = wordTyped.tpe.asPolyType
 
-
             val tvars = for tparam <- polyType.tparams yield TypeVar(tparam.name, this.inferencer)
             val targs = tvars.map(tvar => TypeTree(tvar)(word.span))
             val tpe = TypeOps.substTypeParams(polyType.resultType, tvars)
@@ -162,30 +242,28 @@ class ExprTyper(namer: Namer, checker: Checker, inferencer: Inferencer):
           val tp = wordTyped.tpe
 
           if tp.isProcType then
+            val procType = tp.asProcType
             val precedence = ExprTyper.precedence(wordTyped)
             // infix, postfix, prefix
             if precedence > precLimit then
+              val preTypes = procType.preParamTypes
+              val postTypes = procType.postParamTypes
+              val resultType = procType.resultType
+
+              val preArgs = values.takeRight(preTypes.size).toList
+              values.dropRightInPlace(preTypes.size)
+
+              val (postArgs, rest) = parseMixed(words, precedence).splitAt(postTypes.size)
+              val call = Item.Call(wordTyped, preArgs, postArgs)(preTypes, postTypes, resultType)
+
               // continue if current function has higher binding power
-              values ++= call(wordTyped, tp.asProcType, words, values, precedence)
+              values += call
+              values ++ rest
             else
               // TODO: wordTyped is discarded and it will be checked!
               // put back word
               words.insert(0, word)
               continue = false
-
-          else if wordTyped.isInstanceOf[Ident] && tp.hasApplyMethod then
-            val funType = tp.termMember("apply").asProcType
-            val autoApply = Select(wordTyped, "apply")(funType, wordTyped.span)
-            // prefix
-            if values.isEmpty && words.nonEmpty then
-              val precedence = ExprTyper.precedence(autoApply)
-              if precedence > precLimit then
-                values ++= call(autoApply, funType, words, values, precedence)
-              else
-                values += Item.Typed(wordTyped)
-
-            else
-              values += Item.Typed(wordTyped)
 
           else
             values += Item.Typed(wordTyped)
@@ -195,25 +273,37 @@ class ExprTyper(namer: Namer, checker: Checker, inferencer: Inferencer):
     end while
 
     values.toList
-  end parse
+  end parseMixed
 
-  def call(
-      fun: Word, procType: ProcType,
-      words: mutable.ListBuffer[Ast.Word],
-      values: mutable.ArrayBuffer[Item],
-      precedence: Int)(
-      using Reporter, Scope, Source): List[Item]
-  =
+  /** Parse items from the words with the limit precedence for dotless call syntax */
+  private def parseDotless(words: mutable.ListBuffer[Ast.Word], precLimit: Int)(using rp: Reporter, so: Source): Item =
+    var res = Item.Raw(words.remove(0))
+    var continue = true
 
-    val preTypes = procType.preParamTypes
-    val postTypes = procType.postParamTypes
-    val resultType = procType.resultType
+    // a + b - c * 2
 
-    val preArgs = values.takeRight(preTypes.size).toList
-    values.dropRightInPlace(preTypes.size)
+    while continue && words.nonEmpty do
+      words.head match
+        case ident: Ast.Ident =>
+          // TODO: support prefix operators?
+          val precedence = ExprTyper.precedence(ident.name)
+          // infix
+          if precedence > precLimit then
+            words.remove(0)
+            val rhs = parseDotless(words, precedence)
+            res = Item.InfixCall(res, ident, rhs)
+          else
+            continue = false
 
-    val (postArgs, rest) = parse(words, precedence).splitAt(postTypes.size)
-    Item.Call(fun, preArgs, postArgs)(preTypes, postTypes, resultType) :: rest
+        case word =>
+          continue = false
+          words.clear
+          Reporter.error("A method name expected here in dotless call syntax", word.pos)
+
+    end while
+
+    res
+  end parseDotless
 
   def errorTree(span: Span): Word = Block(Nil)(ErrorType, span)
 end ExprTyper
