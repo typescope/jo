8e0dc9e9c39ba5941dc67fd57c8479f65bed2d26
Fengyun Liu <fengyun.liu.cs@gmail.com>
2025-04-29 16:41:16 +0200

    Update docs

--- stack-lang/ast/Desugaring.scala
+++ stack-lang/ast/Desugaring.scala
@@ -19,6 +19,28 @@ object Desugaring:
       case sec: Section  => synthesizeSection(sec, dataDefs.getOrElse(sec.name, null)) :: Nil
       case defn => defn :: Nil
 
+  /** Desugar a data definition
+    *
+    * Given
+    *
+    *     data A[X, ...](x1: T1, ...)
+    *
+    * It will desugar to
+    *
+    *     type A[X, ...] = #A(x1: X, ...)
+    *
+    *     section A
+    *       fun A[X, ...](x1: T1, ...): A = #A x1 ...
+    *
+    *       pattern A[X, ...](x1: T1, ...): A = case #A x1 ...
+    *
+    * If section `A` already exists, the synthesized members will become members
+    * of the existing section.
+    *
+    * If the member of the same name already exists in section `A`, the
+    * corresponding synthetic member is ignored to prefer the user-defined
+    * member.
+    */
   def synthesizeDataDef(ddef: DataDef, secDef: Section | Null): List[Def] =
     val tagType = TagType(ddef.ident, ddef.params)(ddef.span)
     val tdef = TypeDef(ddef.ident, ddef.tparams, tagType, isBound = false)(ddef.span)
@@ -32,15 +54,20 @@ object Desugaring:
 
   /** Synthesize members for companion section of a data definition
     *
-    *     data A(x1: T1, ...)
+    * Given
     *
-    * will add following members to
+    *     section A
+    *       ...
+    *
+    *     data A[X, ...](x1: T1, ...)
+    *
+    * The following members will be added to section A:
     *
     *
     *     section A
-    *         fun A(x1: T1, ...): A = #A x1 ...
+    *       fun A[X, ...](x1: T1, ...): A = #A x1 ...
     *
-    *         pattern A(x1: T1, ...): A = case #A x1 ...
+    *       pattern A[X, ...](x1: T1, ...): A = case #A x1 ...
     *
     * The synthesis will be skipped if target of the same name already exists.
     */
