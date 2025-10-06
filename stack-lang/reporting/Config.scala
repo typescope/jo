7d6e5881890695ad6e9d5173f4ae8f1d2736da7c
Fengyun Liu <fengyun.liu.cs@gmail.com>
2025-10-06 17:55:27 +0200

    Better handling of relative sast paths

--- stack-lang/reporting/Config.scala
+++ stack-lang/reporting/Config.scala
@@ -43,6 +43,16 @@ object Config:
     "-lib"            -> true,   // -lib <dir>
   )
 
-  val JSRuntimePath = "out/runtime/js"
-  val NativeRuntimePath = "out/native/js"
-  val StdLibPath = "out/stdlib"
+  // Get the root directory of the project (set by the bin/jo wrapper script)
+  private lazy val rootDir: String =
+    sys.env.getOrElse("JO_HOME", System.getProperty("user.dir"))
+
+  // Compute paths relative to project root
+  lazy val JSRuntimePath: String =
+    java.nio.file.Paths.get(rootDir, "sast/runtime/js").toString
+
+  lazy val NativeRuntimePath: String =
+    java.nio.file.Paths.get(rootDir, "sast/runtime/native").toString
+
+  lazy val StdLibPath: String =
+    java.nio.file.Paths.get(rootDir, "sast/stdlib").toString
