406ec50cbfbb2da99411ba649f50d56f8f86c3b0
Fengyun Liu <fengyun.liu.cs@gmail.com>
2025-10-28 11:16:03 +0100

    Fix syntax highlighting

--- docs/js/jo.js
+++ docs/js/jo.js
@@ -5,169 +5,34 @@ hljs.registerLanguage('jo', function(hljs) {
     aliases: ['jo'],
     case_insensitive: false,
     keywords: {
-      keyword: [
-        'def', 'data', 'param', 'type', 'val', 'var', 'match', 'case', 'if', 'then', 'else',
-        'with', 'end', 'import', 'section', 'receives', 'abort', 'return', 'extends'
-      ].join(' '),
-      built_in: [
-        'String', 'Int', 'Bool', 'Unit', 'Any', 'Option', 'List', 'Array'
-      ].join(' '),
-      literal: [
-        'true', 'false', 'None', 'Some', 'Empty'
-      ].join(' ')
+      keyword: 'def data param type val var fun match case if then else with end import namespace section receives pattern allow while do begin auto defer class new alias as',
+      built_in: 'String Int Bool Unit Any Option List Array',
+      literal: 'true false None Some Empty'
     },
     contains: [
       // Line comments
       hljs.COMMENT('//', '$'),
 
-      // Block comments
-      hljs.COMMENT('/\\*', '\\*/'),
+      // Simple block comments for now - just //[ ... //]
+      hljs.COMMENT('//\\[', '//\\]'),
 
-      // String literals
-      {
-        className: 'string',
-        begin: '"',
-        end: '"',
-        contains: [hljs.BACKSLASH_ESCAPE]
-      },
+      // Strings
+      hljs.QUOTE_STRING_MODE,
+      hljs.APOS_STRING_MODE,
 
       // Numbers
-      hljs.NUMBER_MODE,
+      hljs.C_NUMBER_MODE,
 
-      // Type annotations with colon
+      // Tags: #Nil, #Cons, etc.
       {
-        begin: ':\\s*[A-Z][a-zA-Z0-9_]*',
-        returnBegin: true,
-        contains: [
-          {
-            begin: ':',
-            className: 'operator'
-          },
-          {
-            begin: '[A-Z][a-zA-Z0-9_]*',
-            className: 'type'
-          }
-        ]
+        className: 'symbol',
+        begin: '#[A-Z][a-zA-Z0-9_]*'
       },
 
-      // Pattern matching arrow
+      // Operators
       {
-        begin: '=>',
-        className: 'operator'
-      },
-
-      // Pipe for algebraic data types
-      {
-        begin: '\\|',
-        className: 'operator'
-      },
-
-      // Function definitions
-      {
-        begin: '\\bdef\\s+[a-zA-Z_][a-zA-Z0-9_]*',
-        returnBegin: true,
-        contains: [
-          {
-            begin: 'def',
-            className: 'keyword'
-          },
-          {
-            begin: '[a-zA-Z_][a-zA-Z0-9_]*',
-            className: 'title function_'
-          }
-        ]
-      },
-
-      // Data type definitions
-      {
-        begin: '\\bdata\\s+[A-Z][a-zA-Z0-9_]*',
-        returnBegin: true,
-        contains: [
-          {
-            begin: 'data',
-            className: 'keyword'
-          },
-          {
-            begin: '[A-Z][a-zA-Z0-9_]*',
-            className: 'title class_'
-          }
-        ]
-      },
-
-      // Type definitions
-      {
-        begin: '\\btype\\s+[A-Z][a-zA-Z0-9_]*',
-        returnBegin: true,
-        contains: [
-          {
-            begin: 'type',
-            className: 'keyword'
-          },
-          {
-            begin: '[A-Z][a-zA-Z0-9_]*',
-            className: 'title class_'
-          }
-        ]
-      },
-
-      // Constructor patterns in match cases
-      {
-        begin: '\\bcase\\s+[A-Z][a-zA-Z0-9_]*',
-        returnBegin: true,
-        contains: [
-          {
-            begin: 'case',
-            className: 'keyword'
-          },
-          {
-            begin: '[A-Z][a-zA-Z0-9_]*',
-            className: 'title class_'
-          }
-        ]
-      },
-
-      // Context parameters with "receives"
-      {
-        begin: '\\breceives\\s+[a-zA-Z_][a-zA-Z0-9_,\\s]*',
-        returnBegin: true,
-        contains: [
-          {
-            begin: 'receives',
-            className: 'keyword'
-          },
-          {
-            begin: '[a-zA-Z_][a-zA-Z0-9_]*',
-            className: 'variable'
-          }
-        ]
-      },
-
-      // Import statements
-      {
-        begin: '\\bimport\\s+[a-zA-Z0-9_.\\*]+',
-        returnBegin: true,
-        contains: [
-          {
-            begin: 'import',
-            className: 'keyword'
-          },
-          {
-            begin: '[a-zA-Z0-9_.\\*]+',
-            className: 'string'
-          }
-        ]
-      },
-
-      // Variables and identifiers
-      {
-        begin: '\\b[a-z_][a-zA-Z0-9_]*\\b',
-        className: 'variable'
-      },
-
-      // Types and constructors (capitalized)
-      {
-        begin: '\\b[A-Z][a-zA-Z0-9_]*\\b',
-        className: 'type'
+        className: 'operator',
+        begin: '=>|<:|[=:+\\-*/%<>!&|^~]+'
       }
     ]
   };
