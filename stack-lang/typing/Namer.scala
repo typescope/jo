3f7288cd3415b9fba2d6156c3c7fe39b08e35ead
Fengyun Liu <fengyun.liu.cs@gmail.com>
2025-11-11 23:46:31 +0100

    Enrich neg test for varargs

--- stack-lang/typing/Namer.scala
+++ stack-lang/typing/Namer.scala
@@ -812,6 +812,7 @@ class Namer(using Config):
 
           else
             // no adaptation for spread
+            // TODO: maybe we should do the adaptation as well?
             val listType = AppliedType(StaticRef(defn.List_type), elementType :: Nil)
             given TargetType = TargetType.Known(listType, adapters = Nil)
             val argTyped = transform(rest.head)
