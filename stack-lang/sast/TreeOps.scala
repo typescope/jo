ece1f3c5e5b8d467eb8b9d801d91d1100c1de84f
Fengyun Liu <fengyun.liu.cs@gmail.com>
2025-11-12 12:15:53 +0100

    Add TreeOps.etaExpand

--- stack-lang/sast/TreeOps.scala
+++ stack-lang/sast/TreeOps.scala
@@ -2,8 +2,10 @@ package sast
 
 import Trees.*
 import Symbols.Symbol
+import Flags.*
 import Types.*
 
+import ast.Positions.{Span, Source}
 
 import scala.collection.mutable
 
@@ -112,6 +114,69 @@ object TreeOps:
 
         else
           tryAdapters(word, targetType, rest)
+
+  /** Eta-expand a function to an object with an apply method
+    *
+    * Converts: f
+    * To: { def apply[T1, ...](arg1: T1, ...): U = f(arg1, ...) }
+    */
+  def etaExpand(fun: Symbol, policy: Effects.Policy, span: Span)(using Definitions, Source): Word =
+    val procType = fun.info.asProcType
+    val pos = span.toPos
+
+    // Create a "this" symbol for the object
+    val thisSym = Symbol.createSymbol("this", Synthetic, pos)
+
+    // Create an "apply" method symbol
+    val applySym = Symbol.createSymbol("apply", Fun | Method | Synthetic, pos)
+
+    // Create parameter symbols for the apply method
+    val paramSyms =
+      for param <- procType.params yield
+        Symbol.createSymbol(param.name, param.info, Param, applySym, pos)
+
+    // Create auto parameter symbols for the apply method
+    val autoSyms =
+      for auto <- procType.autos yield
+        Symbol.createSymbol(auto.name, auto.info, Context, applySym, pos)
+
+    // No preParam for methods
+    val applyProcType = procType.copy(preParamCount = 0)
+
+    // Build the object type
+    val objType = ObjectType(NamedInfo("apply", applyProcType) :: Nil, mutableFields = Nil)
+
+    // Build the body: call the original function with the parameters
+    val funIdent = Ident(fun)(span)
+
+    // Apply type arguments if polymorphic
+    val funWithTargs =
+      if procType.isPolyType then
+        funIdent.appliedToTypes(procType.tparams.map(StaticRef.apply)*)
+      else
+        funIdent
+
+    // Apply regular arguments
+    val paramIdents = paramSyms.map(sym => Ident(sym)(span))
+    val autoIdents = autoSyms.map(sym => Ident(sym)(span))
+    val body = Apply(funWithTargs, paramIdents, autoIdents)(span)
+
+    // Create the apply method definition - no tparams for the FunDef, they're in the ProcType
+    val resultTypeTree = TypeTree(procType.resultType)(span.point)
+    val adaptersIdents = procType.adapters.map(_.map(s => Ident(s)(span)))
+    val funDef = FunDef(
+      applySym,
+      procType.tparams,
+      paramSyms,
+      adaptersIdents,
+      autoSyms,
+      resultTypeTree,
+      policy,
+      body
+    )(span)
+
+    // Create and return the object
+    Object(thisSym, funDef :: Nil)(objType, span)
 
   /** Returns (locals, free) */
   def variableCensus(fdef: FunDef)(using Definitions): (List[Symbol], List[Symbol]) =
