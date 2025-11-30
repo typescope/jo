dd6ca5d69808018fee6b70751869af554f9e5e00
Fengyun Liu <fengyun.liu.cs@gmail.com>
2025-11-30 21:11:33 +0100

    Support view in native backend

--- stack-lang/native/EncodeClass.scala
+++ stack-lang/native/EncodeClass.scala
@@ -17,18 +17,13 @@ import scala.collection.mutable
   *     {
   *        a = ...,
   *        b = ...,
-  *        foo = ...,
-  *        bar = ...,
   *     }
   *
-  * Note that we duplicate the method table for each instance for simplicity.
-  * We might allow different instances of the same class to have different
-  * bindings for methods, e.g., creating an instance with overridings in
-  * refinement.
+  * Note that all method calls and concrete interface calls are dispatched
+  * statically.
   *
-  *
-  * Class methods are lifted to top-level and augmented with the `this`
-  * parameter.
+  * Class methods and concrete interface methods are lifted to top-level and
+  * augmented with the `this` parameter.
   */
 class EncodeClass(using defn: Definitions) extends phases.Phase[EncodeClass.Context]:
   val contextObject = EncodeClass.CacheContext
@@ -42,7 +37,9 @@ class EncodeClass(using defn: Definitions) extends phases.Phase[EncodeClass.Cont
 
   override def transformDefs(defs: List[Def])(using Context): List[Def] =
     defs.flatMap:
-      case cdef: ClassDef => flatten(cdef)
+      case cdef: ClassDef => flattenClass(cdef)
+
+      case idef: InterfaceDef => flattenInterface(idef)
 
       case defn => super.transformDef(defn) :: Nil
 
@@ -72,7 +69,25 @@ class EncodeClass(using defn: Definitions) extends phases.Phase[EncodeClass.Cont
         ctx.methodToLiftedMap(methodSym) = liftedSym
         liftedSym
 
-  private def flatten(cdef: ClassDef)(using ctx: Context): List[Def] =
+  private def flattenInterface(idef: InterfaceDef)(using ctx: Context): List[Def] =
+    val self = idef.self
+    for fdef <- idef.methods if !fdef.symbol.is(Flags.Defer) yield
+      val liftedSym = getLiftedFunSymbol(fdef.symbol)
+      // TODO: type erasure to properly handle type parameters
+      val body2 =
+        given Context = EncodeClass.CacheContext.newContext(liftedSym, ctx)
+        this.transform(fdef.body)
+
+      FunDef(
+        liftedSym, fdef.tparams,
+        self :: fdef.params, Nil :: fdef.adapters,
+        fdef.autos, fdef.candidates,
+        fdef.resultType,
+        fdef.effectPolicy,
+        body2
+      )(fdef.span)
+
+  private def flattenClass(cdef: ClassDef)(using ctx: Context): List[Def] =
     val self = cdef.self
     for fdef <- cdef.funs yield
       val liftedSym = getLiftedFunSymbol(fdef.symbol)
@@ -96,10 +111,37 @@ class EncodeClass(using defn: Definitions) extends phases.Phase[EncodeClass.Cont
     for field <- classInfo.fields yield
       members += field.name -> Encoded(IntLit(0)(newExpr.span))(field.info)
 
-    for fun <- classInfo.allMethods do
-      members += fun.name -> Ident(getLiftedFunSymbol(fun))(newExpr.span)
-
     Encoded(RecordLit(members.toList)(newExpr.span))(newExpr.tpe)
+
+  override def transformFieldAssign(assign: FieldAssign)(using Context): Word =
+    val FieldAssign(lhs @ Select(qual, name), rhs) = assign
+    val viewFieldType = lhs.tpe
+    viewFieldType match
+      case MemberRef(_, sym) if sym.isAllOf(Flags.View | Flags.Defer) =>
+        // Create interface object, see Memory.encodeInterfaceType
+        val classInfo = sym.owner.classInfo
+        val interfaceInfo = viewFieldType.asClassInfo
+        val members = new mutable.ArrayBuffer[(String, Word)]
+        for meth <- interfaceInfo.methods if meth.is(Flags.Defer) do
+          val memberRef =
+            classInfo.getMemberSymbol(meth.name) match
+              case Some(sym) => getLiftedFunSymbol(sym)
+              case None => throw new Exception("Implementation missing for " + meth + " in class " + sym.owner)
+
+          members += meth.name -> Ident(memberRef)(assign.span)
+        end for
+
+        val vtable = RecordLit(members.toList)(assign.span)
+        val encoding = RecordLit(
+          (Memory.VTABLE, vtable)
+          :: (Memory.UNDERLYING, Ident(classInfo.self)(assign.span))
+          :: Nil
+        )(assign.span)
+
+        assign.copy(rhs = Encoded(encoding)(viewFieldType.widenTermRef))
+
+      case _ =>
+        super.transformFieldAssign(assign)
 
   override def transformApply(apply: Apply)(using ctx: Context): Word =
     val Apply(fun, args, autos) = apply
@@ -107,18 +149,54 @@ class EncodeClass(using defn: Definitions) extends phases.Phase[EncodeClass.Cont
     val args2 = args.map(this.transform)
     val autos2 = autos.map(this.transform)
 
+    /** Deferred interface calls have special semantics */
+    def isAbstractInterfaceMethod(refType: Type): Boolean =
+      refType match
+        case MemberRef(_, sym) => sym.owner.isInterface && sym.is(Flags.Defer)
+        case _ => false
+
+    def getFunTarget(receiverRef: Word, name: String, targs: List[TypeTree]): (Word, Word) =
+      val memberRef = receiverRef.tpe.termMember(name).as[RefType]
+      val isAbstractCall = isAbstractInterfaceMethod(memberRef)
+
+      val procType =
+        if targs.isEmpty then memberRef.asProcType
+        else memberRef.asProcType.instantiate(targs.map(_.tpe))
+
+      val liftedFun =
+        if isAbstractCall then
+          receiverRef.select(name)
+        else
+          Ident(getLiftedFunSymbol(memberRef.symbol))(fun.span)
+
+      val liftedProcType =
+        if isAbstractCall then
+          // The `this` of an abstract interface method is the implementation class
+          procType.prepend(NamedInfo("this", AnyType) :: Nil)
+
+        else
+          procType.prepend(NamedInfo("this", receiverRef.tpe.widen) :: Nil)
+
+      val liftedFunEncoded = Encoded(liftedFun)(liftedProcType)
+
+      val thisObj =
+        if isAbstractCall then
+          val interfaceInfo = memberRef.symbol.owner.classInfo
+          val recordType = Memory.encodeInterfaceType(interfaceInfo)
+          Encoded(receiverRef)(recordType).select(Memory.UNDERLYING)
+        else
+          receiverRef
+
+      (liftedFunEncoded, thisObj)
+
+
     fun match
       case Select(qual, name) if qual.tpe.isClassType =>
         val qual2 = this(qual)
-        val memberRef = qual2.tpe.termMember(name).as[RefType]
-        val procType = memberRef.asProcType
-
-        val liftedFun = Ident(getLiftedFunSymbol(memberRef.symbol))(fun.span)
-        val liftedProcType = procType.prepend(NamedInfo("this", qual2.tpe.widen) :: Nil)
-        val liftedFunEncoded = Encoded(liftedFun)(liftedProcType)
 
         if qual2.isIdempotent then
-          Apply(liftedFunEncoded, qual2 :: args2, autos2)(apply.span)
+          val (fun2, thisObj) = getFunTarget(qual2, name, targs = Nil)
+          Apply(fun2, thisObj :: args2, autos2)(apply.span)
 
         else
           val receiverSym =
@@ -128,23 +206,19 @@ class EncodeClass(using defn: Definitions) extends phases.Phase[EncodeClass.Cont
 
           val receiver = Ident(receiverSym)(qual2.span)
           val assign = Assign(Ident(receiverSym)(qual2.span), qual2)
-          val apply2 = Apply(liftedFunEncoded, receiver :: args2, autos2)(apply.span)
+
+          val (fun2, thisObj) = getFunTarget(receiver, name, targs = Nil)
+          val apply2 = Apply(fun2, thisObj :: args2, autos2)(apply.span)
+
           Block(assign :: apply2 :: Nil)(apply.span)
 
       case TypeApply(sel @ Select(qual, name), targs) if qual.tpe.isClassType =>
         // TODO: after type erasure, the special handling here can be removed
         val qual2 = this(qual)
 
-        val memberRef = qual2.tpe.termMember(name).as[RefType]
-        val procType = memberRef.asProcType
-        val funType = procType.instantiate(targs.map(_.tpe))
-
-        val liftedFun = Ident(getLiftedFunSymbol(memberRef.symbol))(sel.span)
-        val liftedProcType = funType.prepend(NamedInfo("this", qual2.tpe.widen) :: Nil)
-        val liftedFunEncoded = Encoded(liftedFun)(liftedProcType)
-
         if qual2.isIdempotent then
-          Apply(liftedFunEncoded, qual2 :: args2, autos2)(apply.span)
+          val (fun2, thisObj) = getFunTarget(qual2, name, targs)
+          Apply(fun2, thisObj :: args2, autos2)(apply.span)
 
         else
           val receiverSym =
@@ -155,7 +229,9 @@ class EncodeClass(using defn: Definitions) extends phases.Phase[EncodeClass.Cont
           val receiver = Ident(receiverSym)(qual2.span)
           val assign = Assign(Ident(receiverSym)(qual2.span), qual2)
 
-          val apply2 = Apply(liftedFunEncoded, receiver :: args2, autos2)(apply.span)
+          val (fun2, thisObj) = getFunTarget(receiver, name, targs)
+          val apply2 = Apply(fun2, thisObj :: args2, autos2)(apply.span)
+
           Block(assign :: apply2 :: Nil)(apply.span)
 
       case _ =>
--- stack-lang/native/Memory.scala
+++ stack-lang/native/Memory.scala
@@ -1,14 +1,39 @@
 package native
 
-import sast.Constant
+import sast.*
 import sast.Types.*
 import sast.Trees.*
-import sast.Definitions
 
 import ast.Positions.Source
 import native.runtime.NativeRuntime
 
-/** Runtime memory representation of records */
+import scala.collection.mutable
+
+/** Runtime memory representation of records and objects
+  *
+  * Class instances are encoded as follows:
+  *
+  *     {
+  *         a = ... ,
+  *         b = ... ,
+  *     }
+  *
+  * A closure object is encoded as follows:
+  *
+  *     {
+  *         vtable = { foo = ..., bar = ... },
+  *         ftable = { a = ..., b = ... }
+  *     }
+  *
+  * An interface object is encoded as follows:
+  *
+  *     {
+  *         vtable = { foo = ..., bar = ... },
+  *         underlying = ...
+  *     }
+  *
+  * The encoding is implementation details and is subject to change.
+  */
 class Memory(runtime: NativeRuntime)(using defn: Definitions):
   val IntType = defn.IntType
   val AddrType = StaticRef(runtime.Core_Addr)
@@ -23,7 +48,7 @@ class Memory(runtime: NativeRuntime)(using defn: Definitions):
     assert(index >= 0, field + " not found in " + recordType.show)
     index << 2
 
-  def writeField(recordType: RecordType, field: String, ref: Word, rhs: Word)(using Source): Word =
+  def writeMember(recordType: RecordType, field: String, ref: Word, rhs: Word)(using Source): Word =
     val offset = fieldOffset(recordType, field)
     var addr: Word = Encoded(ref)(AddrType)
     if offset != 0 then
@@ -34,7 +59,7 @@ class Memory(runtime: NativeRuntime)(using defn: Definitions):
     val writeIntFun = Ident(runtime.Core_writeInt)(rhs.span)
     writeIntFun.appliedTo(addr, Encoded(rhs)(IntType)).dropValue
 
-  def readField(recordType: RecordType, select: Select)(using Source): Word =
+  def readMember(recordType: RecordType, select: Select)(using Source): Word =
     val Select(qual, field) = select
     val offset = fieldOffset(recordType, field)
     var addr: Word = Encoded(qual)(AddrType)
@@ -47,24 +72,78 @@ class Memory(runtime: NativeRuntime)(using defn: Definitions):
     Encoded(readIntFun.appliedTo(addr))(select.tpe)
 
   def readObjectMember(objType: ObjectType, select: Select)(using Source): Word =
-    val recordType = ObjectEncoding.encodeObjectType(objType)
+    val recordType = Memory.encodeObjectType(objType)
     if select.tpe.isValueType then
-      val tableType = recordType.termMember(ObjectEncoding.FTABLE).asRecordType
-      val tableSelect = Select(Encoded(select.qual)(recordType), ObjectEncoding.FTABLE)(select.span)
-      val table = readField(recordType, tableSelect)
+      val tableType = recordType.termMember(Memory.FTABLE).asRecordType
+      val tableSelect = Select(Encoded(select.qual)(recordType), Memory.FTABLE)(select.span)
+      val table = readMember(recordType, tableSelect)
       val fieldSelect = Select(table, select.name)(select.span)
-      readField(tableType, fieldSelect)
+      readMember(tableType, fieldSelect)
 
     else
-      val tableType = recordType.termMember(ObjectEncoding.VTABLE).asRecordType
-      val tableSelect = Select(Encoded(select.qual)(recordType), ObjectEncoding.VTABLE)(select.span)
-      val table = readField(recordType, tableSelect)
+      val tableType = recordType.termMember(Memory.VTABLE).asRecordType
+      val tableSelect = Select(Encoded(select.qual)(recordType), Memory.VTABLE)(select.span)
+      val table = readMember(recordType, tableSelect)
       val methodSelect = Select(table, select.name)(select.span)
-      readField(tableType, methodSelect)
+      readMember(tableType, methodSelect)
 
-  def writeObjectField(objType: ObjectType, field: String, ref: Word, rhs: Word)(using Source): Word =
-    val recordType = ObjectEncoding.encodeObjectType(objType)
-    val tableType = recordType.termMember(ObjectEncoding.FTABLE).asRecordType
-    val tableSelect = Select(Encoded(ref)(recordType), ObjectEncoding.FTABLE)(rhs.span)
-    val table = readField(recordType, tableSelect)
-    writeField(tableType, field, table, rhs)
+  def writeObjectMember(objType: ObjectType, field: String, ref: Word, rhs: Word)(using Source): Word =
+    val recordType = Memory.encodeObjectType(objType)
+    val tableType = recordType.termMember(Memory.FTABLE).asRecordType
+    val tableSelect = Select(Encoded(ref)(recordType), Memory.FTABLE)(rhs.span)
+    val table = readMember(recordType, tableSelect)
+    writeMember(tableType, field, table, rhs)
+
+  def writeClassMember(classInfo: ClassInfo, member: String, ref: Word, rhs: Word)(using Source): Word =
+    val recordType = Memory.encodeClassType(classInfo)
+    assert(classInfo.termMember(member).isValueType, "Expect value type, found = " + classInfo.termMember(member).show)
+    writeMember(recordType, member, ref, rhs)
+
+  def readClassMember(classInfo: ClassInfo, select: Select)(using Source): Word =
+    val recordType = Memory.encodeClassType(classInfo)
+    assert(select.tpe.isValueType, "Expect value type, found = " + select.tpe.show)
+    readMember(recordType, select)
+
+  def readInterfaceMember(interfaceInfo: ClassInfo, select: Select)(using Source): Word =
+    val recordType = Memory.encodeInterfaceType(interfaceInfo)
+    assert(select.tpe.isProcType, "Expect proc type, found = " + select.tpe.show)
+
+    val tableType = recordType.termMember(Memory.VTABLE).asRecordType
+    val tableSelect = Select(Encoded(select.qual)(recordType), Memory.VTABLE)(select.span)
+    val table = readMember(recordType, tableSelect)
+    val methodSelect = Select(table, select.name)(select.span)
+    readMember(tableType, methodSelect)
+
+object Memory:
+  val VTABLE = "vtable"
+  val FTABLE = "ftable"
+  val UNDERLYING = "underlying"
+
+  def encodeObjectType(objType: ObjectType): RecordType =
+    val ftable = RecordType(objType.fields)
+    val vtable = RecordType(objType.methods)
+    RecordType(NamedInfo(VTABLE, vtable) :: NamedInfo(FTABLE, ftable) :: Nil)
+
+  def encodeClassType(classInfo: ClassInfo)(using Definitions): RecordType =
+    val memberTypes = new mutable.ArrayBuffer[NamedInfo[Type]]
+    for field <- classInfo.fields do
+      memberTypes += field.toNamedInfo
+
+    RecordType(memberTypes.toList)
+
+  def encodeInterfaceType(interfaceInfo: ClassInfo)(using Definitions): RecordType =
+    val memberTypes = new mutable.ArrayBuffer[NamedInfo[Type]]
+    for meth <- interfaceInfo.methods if meth.is(Flags.Defer) do
+      memberTypes += meth.toNamedInfo
+
+    val vtable = RecordType(memberTypes.toList)
+    RecordType(NamedInfo(VTABLE, vtable) :: NamedInfo(UNDERLYING, AnyType) :: Nil)
+
+  def encodeObject(obj: RecordLit)(using Definitions): Word =
+    val fields = obj.args.filter { case (name, rhs) => rhs.tpe.isValueType }
+    val ftable = RecordLit(fields)(obj.span)
+
+    val methods = obj.args.filter { case (name, rhs) => rhs.tpe.isProcType }
+    val vtable = RecordLit(methods)(obj.span)
+
+    RecordLit(List(Memory.VTABLE -> vtable, Memory.FTABLE -> ftable))(obj.span)
