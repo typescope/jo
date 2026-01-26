ab2c584319cf57f76293c46c812404dc1f77067a
Fengyun Liu <fengyun.liu.cs@gmail.com>
2026-01-26 21:43:48 +0100

    Fix unused warning

--- stack-lang/typing/PatternTyper.scala
+++ stack-lang/typing/PatternTyper.scala
@@ -290,7 +290,7 @@ class PatternTyper(namer: Namer)(using Config):
         else
           if sym == defn.Predef_orPattern then
             assert(args.size == 2, "args.size = " + args.size)
-            transformOrPattern(args(0), args(1), scrutType, patSpan)
+            transformOrPattern(args(0), args(1), scrutType)
 
           else if sym == defn.Predef_andPattern then
             assert(args.size == 2, "args.size = " + args.size)
@@ -320,7 +320,7 @@ class PatternTyper(namer: Namer)(using Config):
       WildcardPattern()(ErrorType, patSpan)
 
 
-  private def transformOrPattern(lhs: Ast.Pattern, rhs: Ast.Pattern, scrutType: Type, patSpan: Span)
+  private def transformOrPattern(lhs: Ast.Pattern, rhs: Ast.Pattern, scrutType: Type)
       (using defn: Definitions, sc: FlowScope, rp: Reporter, so: Source, tvars: TypeVars)
   : Pattern =
     given rp2: Reporter = rp.fresh(buffer = true)
