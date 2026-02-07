ebc072ec40a40f273aeb4839ef5f436f336c4f55
Fengyun Liu <fengyun.liu.cs@gmail.com>
2026-02-07 16:16:15 +0100

    Enhance extension type check

--- stack-lang/typing/Extensions.scala
+++ stack-lang/typing/Extensions.scala
@@ -36,19 +36,23 @@ object Extensions:
 
     if procType.preParamCount == 0 then
       Reporter.error(s"Method ${sym.name} is not an extension method", pos)
+      return false
+
+    if procType.preParamCount > 1 then
+      Reporter.error(s"Extension method ${sym.name} must have exactly one pre-parameter", pos)
+      return false
+
+    // Check that the base type conforms to the method's pre-parameter type.
+    // For polymorphic extensions, instantiate type parameters with fresh type
+    // vars so that unification can determine them from the base type.
+    val preParamType = procType.preParamTypes.head
+    given tvars: TypeVars = new UnificationSolver
+    val map = new TypeOps.InstantiateTypeParam(pos.span)
+    val preParamTypeFlex = map(preParamType)(using ())
+    if !Subtyping.conforms(baseType, preParamTypeFlex) then
+      Reporter.error(
+        s"Base type ${baseType.show} does not conform to parameter type ${preParamType.show} of extension method ${sym.name}",
+        pos)
       false
     else
-      // Check that the base type conforms to the method's pre-parameter type.
-      // For polymorphic extensions, instantiate type parameters with fresh type
-      // vars so that unification can determine them from the base type.
-      val preParamType = procType.preParamTypes.head
-      given tvars: TypeVars = new UnificationSolver
-      val map = new TypeOps.InstantiateTypeParam(pos.span)
-      val preParamTypeFlex = map(preParamType)(using ())
-      if !Subtyping.conforms(baseType, preParamTypeFlex) then
-        Reporter.error(
-          s"Base type ${baseType.show} does not conform to parameter type ${preParamType.show} of extension method ${sym.name}",
-          pos)
-        false
-      else
-        true
+      true
